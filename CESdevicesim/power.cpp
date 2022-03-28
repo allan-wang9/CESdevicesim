9d3f013224f3a209de540feb81bd124cc081d9af
MartinMacharia <57692374+Marsh90@users.noreply.github.com>
2022-03-28 20:41:26 +0300

    Update power.cpp

--- CESdevicesim/power.cpp
+++ CESdevicesim/power.cpp
@@ -2,5 +2,11 @@
 
 power::power()
 {
-
+    this->batteryCharge = batteryCharge;
 }
+
+//getter for batteryCharge
+int power::getBatteryCharge() const {return batteryCharge;}
+
+//seter for batteryCharge
+void power::setBatteryCharge(int charge) {batteryCharge = charge;}
