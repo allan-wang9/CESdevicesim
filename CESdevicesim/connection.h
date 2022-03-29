fcdc221e3b53ed7b0b54a0941aab755a9e4351cd
MartinMacharia <57692374+Marsh90@users.noreply.github.com>
2022-03-29 16:52:58 +0300

    Update connection.h

--- CESdevicesim/connection.h
+++ CESdevicesim/connection.h
@@ -1,13 +1,21 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
+#include "power.h"
 
-class connection
-{
+class connection{
+
+private:
+    power devicePower;
+    bool connectionStatus;
+    int connectionLevel;
+
 public:
     connection();
     int testConnection();
     int resetConnection();
+    int getConnectionLevel() const;
+    void setConnectionLevel(int);
 };
 
 #endif // CONNECTION_H
