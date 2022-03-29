15268b4c22675e37c078708b2c94b2f8af8505b5
MartinMacharia <57692374+Marsh90@users.noreply.github.com>
2022-03-29 16:53:40 +0300

    Update connection.cpp

--- CESdevicesim/connection.cpp
+++ CESdevicesim/connection.cpp
@@ -1,6 +1,29 @@
 #include "connection.h"
+#include "power.h"
 
-connection::connection()
-{
+//constructor
+connection::connection(){
+    this->connectionStatus = false;
+    this->connectionLevel = connectionLevel;
 
 }
+
+//testConnection() -> return 0 if an error is detected (no 9V battery), otherwise return 1
+int connection::testConnection(){
+    if (devicePower.getBatteryCharge() <= 0){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+//resetConnection -> return 0 if connection cannot be reset, otherwise return 1
+int connection::resetConnection(){
+    return 1; //will require more info on what determines a "reset"
+}
+
+//getter for connectionLevel
+int connection::getConnectionLevel() const {return connectionLevel;}
+
+//seter for connectionLevel
+void connection::setConnectionLevel(int connection) {connectionLevel = connection;}
